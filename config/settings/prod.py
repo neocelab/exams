602e1e234ed268cfd5985f270d0ead007521a328
이경복 <dicsys2@gmail.com>
2023-04-30 13:43:00 +0900

    6차 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,10 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.209.232.84']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
